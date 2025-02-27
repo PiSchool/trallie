aba030a7e1a15ed8b74f5fe31cc1866075c69139
Vijayasri I. <thisisvij98@gmail.com>
2025-02-27 23:04:01 +0530

    feat: add support for openai provider

---
+++ trallie/providers/openai.py
@@ -0,0 +1,81 @@
+import os
+from functools import lru_cache
+from typing import Any
+
+import openai
+
+from trallie.providers import (
+    BaseProvider,
+    ProviderInitializationError,
+    register_provider,
+)
+
+def openai_api_call(default_return_value: Any):
+    def decorator(func):
+        def wrapper(self, *args, **kwargs):
+            try:
+                return func(self, *args, **kwargs)
+            except (
+                openai.error.Timeout,
+                openai.error.APIError,
+                openai.error.APIConnectionError,
+                openai.error.InvalidRequestError,
+                openai.error.AuthenticationError,
+                openai.error.PermissionError,
+                openai.error.RateLimitError
+            ) as e:
+                if isinstance(e, openai.error.Timeout):
+                    print("[openai] API request timed out: {e}.")
+                if isinstance(e, openai.error.APIError):
+                    print("[openai] API returned an API Error: {e}.")
+                if isinstance(e, openai.error.APIConnectionError):
+                    print("[openai] API request failed to connect: {e}.")
+                if isinstance(e, openai.error.InvalidRequestError):
+                    print("[openai] API request was invalid: {e}.")
+                if isinstance(e, openai.error.AuthenticationError):
+                    print("[openai] API request was not authorized: {e}.")
+                if isinstance(e, openai.error.PermissionError):
+                    print("[openai] API request was not permitted: {e}.")
+                if isinstance(e, openai.error.RateLimitError):
+                    print("[openai] API request exceeded rate limit: {e}.")
+                return default_return_value
+
+        return wrapper
+
+    return decorator
+
+@register_provider("openai")
+class OpenAIProvider(BaseProvider):
+    def __init__(self) -> None:
+        super().__init__()
+        if "OPENAI_API_KEY" not in os.environ:
+            raise ProviderInitializationError(
+                "Must set the OPENAI_API_KEY environment variable to use the "
+                "'openai' provider."
+            )
+        self.client = openai.OpenAI(
+            api_key=os.environ["OPENAI_API_KEY"], timeout=3.0, max_retries=2
+        )
+
+    @openai_api_call(default_return_value=[])
+    @lru_cache
+    def list_available_models(self) -> list[str]:
+        return [
+            model.id
+            for model in self.client.models.list().data
+            if model.active
+        ]
+
+    @openai_api_call(default_return_value="")
+    @lru_cache
+    def do_chat_completion(self, prompt: str, model_name: str) -> str:
+        chat_completion = self.client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            model=model_name,
+        )
+        return chat_completion.choices[0].message.content
