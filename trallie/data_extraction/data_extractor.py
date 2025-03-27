39821d6ad1b12e918e92ccb378da4740d26dad95
Vijayasri I. <thisisvij98@gmail.com>
2025-03-27 12:32:02 +0530

    modify user prompt

--- trallie/data_extraction/data_extractor.py
+++ trallie/data_extraction/data_extractor.py
@@ -14,19 +14,21 @@ class DataExtractor:
         self.provider = provider
         self.model_name = model_name
         self.client = get_provider(self.provider)
-        self.system_prompt = system_prompt or ZERO_SHOT_EXTRACTION_SYSTEM_PROMPT
-    
-    def extract_data(self, schema, record, max_retries=3):
-        record = DataHandler(record).get_text()
+        self.system_prompt = system_prompt or FEW_SHOT_EXTRACTION_SYSTEM_PROMPT
+
+    def extract_attributes(self, schema, record, max_retries=3):
+        """
+        Extracts attributes for a given record and schema.
+        """
         user_prompt = f"""
-            Following is the record: {record} and the attribute schema for extraction: {schema}
+            Following is the attribute schema for extraction: {schema} and the record: {record}
             Provide the extracted attributes. Avoid any words at the beginning and end.
         """
         for attempt in range(max_retries):
-            try: 
+            try:
                 response = self.client.do_chat_completion(
                     self.system_prompt, user_prompt, self.model_name
-                )  
+                )
                 # Validate if response is a valid JSON
                 response = json.loads(response)
                 return response
@@ -37,3 +39,10 @@ class DataExtractor:
             except Exception as e:
                 print(f"Error: {e}")
                 return None
+
+    def extract_data(self, schema, record, max_retries=3):
+        """
+        Processes record and returns extracted attributes.
+        """
+        record_text = DataHandler(record).get_text()
+        return self.extract_attributes(schema, record_text, max_retries)
