65dd34e5f6ac2cd143cd7c1e40e5bc357c55b6a9
Vijayasri I. <thisisvij98@gmail.com>
2025-04-04 15:24:21 +0530

    add multilingual prompts

--- trallie/prompts/__init__.py
+++ trallie/prompts/__init__.py
@@ -2,7 +2,7 @@ from trallie.prompts.en.data_extraction_prompts import (
     FEW_SHOT_EXTRACTION_SYSTEM_PROMPT,
     ZERO_SHOT_EXTRACTION_SYSTEM_PROMPT,
 )
-from trallie.prompts.en.schema_gen_prompts import (
+from trallie.prompts.en.schema_generation_prompts import (
     FEW_SHOT_GENERATION_SYSTEM_PROMPT,
     ZERO_SHOT_GENERATION_SYSTEM_PROMPT,
     FEW_SHOT_GENERATION_LONG_DOCUMENT_SYSTEM_PROMPT,
