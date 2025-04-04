fc7bebe05b9c91ca4d1ae0d59f6467661bd2806f
Vijayasri I. <thisisvij98@gmail.com>
2025-04-04 12:33:03 +0530

    add language specific folders

--- trallie/prompts/__init__.py
+++ trallie/prompts/__init__.py
@@ -1,8 +1,8 @@
-from trallie.prompts.data_extraction_prompts import (
+from trallie.prompts.en.data_extraction_prompts import (
     FEW_SHOT_EXTRACTION_SYSTEM_PROMPT,
     ZERO_SHOT_EXTRACTION_SYSTEM_PROMPT,
 )
-from trallie.prompts.schema_gen_prompts import (
+from trallie.prompts.en.schema_gen_prompts import (
     FEW_SHOT_GENERATION_SYSTEM_PROMPT,
     ZERO_SHOT_GENERATION_SYSTEM_PROMPT,
     FEW_SHOT_GENERATION_LONG_DOCUMENT_SYSTEM_PROMPT,
