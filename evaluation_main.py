fb9f4e1c1811123945775d74d50e2c60979477d4
Vijayasri I. <thisisvij98@gmail.com>
2025-04-11 15:51:11 +0530

    add refactored eval functions

--- evaluation_main.py
+++ evaluation_main.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-from trallie.evaluation.evaluation import evaluate_openie_sbert, evaluate_value_f1
+from trallie.evaluation.evaluation import evaluate_openie, evaluate_closedie
 from trallie.evaluation.evaluation_params import get_evaluation_params, get_dataset_schema
 from trallie.wrappers import openie, closedie
 
@@ -16,10 +16,10 @@ datasets = get_evaluation_params(base_path)
 for dataset_name, dataset_info in datasets.items():
     dataset_info["schema"] = get_dataset_schema(dataset_info["ground_truth"])
     records = glob.glob(str(dataset_info["files"]))
-    extracted_jsons = openie(dataset_info['description'], records, provider="openai", model_name="gpt-4o", dataset_name=dataset_name)
-    result = evaluate_openie_sbert(dataset_info['ground_truth'], f"{dataset_name}_openie_predicted_table.json")
+    extracted_jsons = openie(dataset_info['description'], records, provider="groq", model_name="llama-3.3-70b-versatile", dataset_name=dataset_name)
+    result = evaluate_openie(dataset_info['ground_truth'], f"{dataset_name}_openie_predicted_table.json")
     print(result)
     schema = get_dataset_schema(dataset_info['ground_truth'])
-    extracted_jsons = closedie(records, schema, provider="openai", model_name="gpt-4o", dataset_name=dataset_name)
-    result = evaluate_value_f1(dataset_info["ground_truth"], f"{dataset_name}_closedie_predicted_table.json")
+    extracted_jsons = closedie(records, schema, provider="groq", model_name="llama-3.3-70b-versatile", dataset_name=dataset_name)
+    result = evaluate_closedie(dataset_info["ground_truth"], f"{dataset_name}_closedie_predicted_table.json")
     print(result)
