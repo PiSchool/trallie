22aa25de2c11507a291e71e95a9ddb4bd8d5b838
Vijayasri I. <thisisvij98@gmail.com>
2025-04-11 16:44:52 +0530

    add method to upload file to s3

--- evaluation_main.py
+++ evaluation_main.py
@@ -5,10 +5,17 @@ from trallie.evaluation.evaluation import evaluate_openie, evaluate_closedie
 from trallie.evaluation.evaluation_params import get_evaluation_params, get_dataset_schema
 from trallie.wrappers import openie, closedie
 
+import boto3
+
 from pathlib import Path
 
 os.environ["GROQ_API_KEY"] = None
 os.environ["OPENAI_API_KEY"] = None 
+
+def upload_to_s3(json_file, bucket_name, s3_key):
+    s3 = boto3.client("s3")
+    s3.upload_file(json_file, bucket_name, s3_key)
+    print(f"✅ Uploaded {json_file} to s3://{bucket_name}/{s3_key}")
 
 base_path = Path("data/evaluation/evaporate/data")
 datasets = get_evaluation_params(base_path)
@@ -23,3 +30,4 @@ for dataset_name, dataset_info in datasets.items():
     extracted_jsons = closedie(records, schema, provider="groq", model_name="llama-3.3-70b-versatile", dataset_name=dataset_name)
     result = evaluate_closedie(dataset_info["ground_truth"], f"{dataset_name}_closedie_predicted_table.json")
     print(result)
+    upload_to_s3(json_file=f"{dataset_name}_openie_results.json", bucket_name="trallie-synth-datagen", s3_key=f"results/{dataset_name}_openie_results.json")
