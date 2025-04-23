f2ee083fc2e6427a647c4665bcda15bf1d18e288
Vijayasri I. <thisisvij98@gmail.com>
2025-04-23 12:57:31 +0530

    modify eval loop

--- evaluation_main.py
+++ evaluation_main.py
@@ -7,8 +7,8 @@ from trallie.wrappers import openie, closedie
 
 from pathlib import Path
 
-os.environ["GROQ_API_KEY"] = None
-os.environ["OPENAI_API_KEY"] = None 
+# os.environ["GROQ_API_KEY"] = None
+# os.environ["OPENAI_API_KEY"] = None 
 
 base_path = Path("data/evaluation/evaporate/data")
 datasets = get_evaluation_params(base_path)
@@ -17,17 +17,17 @@ model_names = ["llama-3.3-70b-versatile", "gpt-4o", "llama-3.1-8b-instant", "dee
 model_providers = ["groq", "openai", "groq", "groq"]
 reasoning_modes = [False, False, False, True]
 
-for dataset_name, dataset_info in datasets.items()[:1]:
+for dataset_name, dataset_info in list(datasets.items())[:1]:
     for provider, model_name, reasoning_mode in zip(model_providers, model_names, reasoning_modes):
-        if any(os.path.exists(f"{model_name}_{dataset_name}_{mode}_predicted_table.json") for mode in ["openie", "closedie"]):
-            continue
         dataset_info["schema"] = get_dataset_schema(dataset_info["ground_truth"])
         records = glob.glob(str(dataset_info["files"]))
-        extracted_jsons = openie(dataset_info['description'], records, provider=provider, model_name=model_name, reasoning_mode=reasoning_mode, dataset_name=dataset_name)
-        result = evaluate_openie(dataset_info['ground_truth'], f"{model_name}_{dataset_name}_openie_predicted_table.json")
-        print(result)
-        schema = get_dataset_schema(dataset_info['ground_truth'])
-        extracted_jsons = closedie(records, schema, provider=provider, model_name=model_name, reasoning_mode=reasoning_mode, dataset_name=dataset_name)
-        result = evaluate_closedie(dataset_info["ground_truth"], f"{model_name}_{dataset_name}_closedie_predicted_table.json")
-        print(result)
+        if not os.path.exists(f"{model_name}_{dataset_name}_openie_predicted_table.json"):
+            extracted_jsons = openie(dataset_info['description'], records, provider=provider, model_name=model_name, reasoning_mode=reasoning_mode, dataset_name=dataset_name)
+        result = evaluate_openie(dataset_info['ground_truth'], f"{model_name}_{dataset_name}_openie_predicted_table.json", exact_match=False)
+        print(f"Results for OpenIE {model_name} : {result}")
+        if not os.path.exists(f"{model_name}_{dataset_name}_closedie_predicted_table.json"):
+            schema = get_dataset_schema(dataset_info['ground_truth'])
+            extracted_jsons = closedie(records, schema, provider=provider, model_name=model_name, reasoning_mode=reasoning_mode, dataset_name=dataset_name)
+        result = evaluate_closedie(dataset_info["ground_truth"], f"{model_name}_{dataset_name}_closedie_predicted_table.json", exact_match=False)
+        print(f"Results for ClosedIE {model_name} : {result}")
     
